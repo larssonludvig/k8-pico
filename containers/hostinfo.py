2549cce3581342414d3c0cc3d7ea13c8d9117c9e
OscarBohlin <oscar@bohlin.se>
2024-09-24 10:25:48 +0200

    basic container post

---
+++ containers/hostinfo.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, Response
+import threading
+
+app = Flask(__name__)
+
+global_data = {}
+global_lock = threading.Lock()
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+	if request.method == "POST":
+		data = request.get_json()
+		hostname = data["hostname"]
+		ip = request.remote_addr
+		with global_lock:
+			global_data.setdefault(hostname, [])
+			global_data[hostname].append((ip))
+
+		print("Stored data")
+		print(f"hostname: {hostname}, ip: {ip}")
+
+	return Response(status=200)
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
--- containers/post_hostinfo.py
+++ containers/post_hostinfo.py
@@ -2,25 +2,32 @@ import requests
 import json
 import socket
 import time
+import os
 
 def main():
-	
-	api_url = "http://localhost/TBD"
-	headers = {"Content-Type":"application/json"}
-	while True:
-		hostname = socket.gethostname()
-		ip = socket.gethostbyname(hostname)
-		data = {
-			"hostname" : hostname,
-			"ip" : ip,
-		}
+    
+    api_url = os.environ.get("K8_DEBUG_TARGET_API")
+    if api_url is None:
+        print("Target API was not specified, nothing to do. Quitting")
+        return
+    
+    headers = {"Content-Type":"application/json"}
+    hostname = socket.gethostname()
 
-		try:
-			requests.post(api_url, data=json.dumps(data), headers=headers)
-		except:
-			pass
-		time.sleep(4)
+    data = {
+        "hostname" : hostname,
+    }
+
+    while True:
+        print("Sending data: ", end="")
+        try:
+            requests.post(api_url, data=json.dumps(data), headers=headers)
+            print("Success!")
+        except:
+            print(f"Failed POST to {api_url}")
+            pass
+        time.sleep(4)
 
 
 if __name__ == "__main__":
-	main()
+    main()
