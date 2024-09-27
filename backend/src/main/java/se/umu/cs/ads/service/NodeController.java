ef4406f0ac8174215283d5be4929b8e6a61b61e0
larssonludvig <ludviglarsson01@live.se>
2024-09-27 11:04:04 +0200

    Added endpoints for Node information

---
+++ backend/src/main/java/se/umu/cs/ads/service/NodeController.java
@@ -0,0 +1,54 @@
+package se.umu.cs.ads.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import se.umu.cs.ads.nodemanager.NodeManager;
+import se.umu.cs.ads.types.Node;
+
+@RestController
+@RequestMapping("api")
+public class NodeController {
+    private NodeManager nodeManager;
+
+    public NodeController() {
+        nodeManager = new NodeManager();
+        System.out.print("What is the name of this node? ");
+        String line = System.console().readLine();
+
+        try {
+            nodeManager.start("k8-pico", line);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("node")
+    public ResponseEntity<Node> getCurrentNode() {
+        Node node = nodeManager.getNode();
+        if (node == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        return ResponseEntity.status(HttpStatus.OK).body(node);
+    }
+
+    @GetMapping("node/{name}")
+    public ResponseEntity<Node> getNode(@PathVariable String name) {
+        Node node = nodeManager.getNode(name);
+        if (node == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        return ResponseEntity.status(HttpStatus.OK).body(node);
+    }
+
+    @GetMapping("nodes")
+    public ResponseEntity<List<Node>> getNodes() {
+        List<Node> nodes = nodeManager.getNodes();
+        if (nodes == null || nodes.size() <= 0)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        return ResponseEntity.status(HttpStatus.OK).body(nodes);
+    }
+}
--- backend/src/main/java/se/umu/cs/ads/service/RESTManager.java
+++ backend/src/main/java/se/umu/cs/ads/service/RESTManager.java
@@ -2,9 +2,11 @@ package se.umu.cs.ads.service;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class RESTManager {
-	private final RESTController controller;
-	public RESTManager() {
-		controller = new RESTController();
-	}
+	// Note: This should not be needed, Spring boot automatically scans for controllers
+	// private final RESTController controller;
+	// public RESTManager() {
+	// 	controller = new RESTController();
+	// }
 }
