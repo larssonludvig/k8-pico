567088040d79601eaba1ac8f03f3a93673e54a60
larssonludvig <ludviglarsson01@live.se>
2024-10-01 10:00:10 +0200

    Refactoring

--- frontend/Types/Node.cs
+++ frontend/Types/Node.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 namespace Types {
     public class Node {
         public string Name      { get; set; } = "";
-        public string? Address   { get; set; } = null;
-        public string? Port      { get; set; } = null;
+        public string? Address  { get; set; } = null;
+        public string? Port     { get; set; } = null;
         public string Cluster   { get; set; } = "";
-        // public List<Pod> pods   { get; set; } = default;
+        public List<Pod> pods   { get; set; } = new List<Pod>();
     }
 }
--- frontend/Types/Pod.cs
+++ frontend/Types/Pod.cs
@@ -2,10 +2,10 @@ using System.Collections.Generic;
 
 namespace Types {
     public class Pod {
-        public string Id = "";
-        public string Name = "";
-        public string Image = "";
-        public Dictionary<int, int> Ports = new Dictionary<int, int>();
-        public List<String> Env = new List<String>();
+        public string Id                    { get; set; } = "";
+        public string Name                  { get; set; } = "";
+        public string Image                 { get; set; } = "";
+        public Dictionary<int, int> Ports   { get; set; } = new Dictionary<int, int>();
+        public List<String> Env             { get; set; } = new List<String>();
     }
 }
