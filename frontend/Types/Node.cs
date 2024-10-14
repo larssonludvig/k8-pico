2bfb22008454ba046a1b6d1045677b422fdf417e
larssonludvig <ludviglarsson01@live.se>
2024-10-14 21:26:11 +0200

    Small fixes to frontend

--- frontend/Types/Node.cs
+++ frontend/Types/Node.cs
@@ -1,11 +1,12 @@
-using System.Collections.Generic;
-
 namespace Types {
     public class Node {
-        public string Name      { get; set; } = "";
-        public string? Address  { get; set; } = null;
-        public int? Port        { get; set; } = null;
-        public string Cluster   { get; set; } = "";
+        public PicoAddress? Address             { get; set; } = null;
+        public string Cluster                   { get; set; } = "";
         public List<PicoContainer> Containers   { get; set; } = new List<PicoContainer>();
+    }
+
+    public class PicoAddress {
+        public string Ip    { get; set; } = "";
+        public int Port     { get; set; } = 0;
     }
 }
